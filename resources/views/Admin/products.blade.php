b38bca0f818efec72dd470f370d9843fdfbdea17
nadia356 <110473332+nadia356@users.noreply.github.com>
2024-04-03 17:50:23 -0600

    d

--- resources/views/Admin/products.blade.php
+++ resources/views/Admin/products.blade.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Products</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         #a {
             color: white;
             background-color: rgb(0, 2, 98) !important;
         }
-    
+
         #a:hover {
             color: white;
             background-color: rgb(40, 43, 250) !important;
         }
-    
+
         th {
             color: white !important;
             background-color: rgb(0, 2, 98) !important;
         }
-    
+
         .text-muted {
             margin-right: 15px;
         }
-    
+
         .pagination .page-link {
             color: white !important;
             background-color: rgb(0, 2, 98) !important;
             font-size: 0.75rem !important;
             /* Tamaño de fuente más pequeño */
         }
-    
+
         .pagination .page-link svg {
             color: white !important;
             background-color: rgb(0, 2, 98) !important;
@@ -43,7 +43,7 @@
             height: 1em !important;
             /* Altura más pequeña */
         }
-    
+
         .pagination .page-item.active .page-link {
             color: white !important;
             background-color: rgb(0, 2, 98) !important;
@@ -51,6 +51,7 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="d-flex mt-4 ">
@@ -58,23 +59,23 @@
         </div>
         <h1 class="text-center">Products</h1>
         @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
         @elseif (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
         @endif
         <div class="d-flex justify-content-end">
             <button type="button" id="a" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#createModal">Create new product</button>
-         </div>
-         @if($products->isEmpty())
-            <div class="alert alert-warning text-center">
-                There are no products
-            </div>
+        </div>
+        @if($products->isEmpty())
+        <div class="alert alert-warning text-center">
+            There are no products
+        </div>
         @else
-         <div class="table-responsive">
+        <div class="table-responsive">
             <table class="table table-striped table-bordered rounded">
                 <thead>
                     <tr>
@@ -87,27 +88,27 @@
                 </thead>
                 <tbody>
                     @foreach ($products as $product)
-                        <tr>
-                            <td class="text-center">{{$product->name}}</td>
-                            <td class="text-center">{{$product->description}}</td>
-                            {{-- Formato de peso mexicano con centavos --}}
-                            <td class="text-center">${{number_format($product->price, 2, '.', ',')}}</td>
+                    <tr>
+                        <td class="text-center">{{$product->name}}</td>
+                        <td class="text-center">{{$product->description}}</td>
+                        {{-- Formato de peso mexicano con centavos --}}
+                        <td class="text-center">${{number_format($product->price, 2, '.', ',')}}</td>
 
-                            <td class="text-center">{{$product->category}}</td>
-                            <td class="text-center">
-                                <button type="button" class="btn btn-primary  btn-sm edit-btn" onclick="openEditModal(this)" data-productid="{{$product->id}}" data-productname="{{$product->name}}" data-productdesc="{{$product->description}}" data-productprice="{{$product->price}}" data-categoryname="{{$product->category}}">Update</button>
-                                
-                                <button type="button" class="btn btn-danger btn-sm edit-btn" data-productid="{{$product->id}}" onclick="openDeleteModal(this)" data->Delete</button>
-                            </td>
-                        </tr>
+                        <td class="text-center">{{$product->category}}</td>
+                        <td class="text-center">
+                            <button type="button" class="btn btn-primary  btn-sm edit-btn" onclick="openEditModal(this)" data-productid="{{$product->id}}" data-productname="{{$product->name}}" data-productdesc="{{$product->description}}" data-productprice="{{$product->price}}" data-categoryname="{{$product->category}}">Update</button>
+
+                            <button type="button" class="btn btn-danger btn-sm edit-btn" data-productid="{{$product->id}}" onclick="openDeleteModal(this)" data->Delete</button>
+                        </td>
+                    </tr>
                     @endforeach
-                </tbody> 
-         </div>
+                </tbody>
+        </div>
     </div>
     <div class="d-flex justify-content-center">
         {{$products->links('pagination::bootstrap-5')}}
     </div>
-            @endif
+    @endif
     {{-- Modal create product --}}
     <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -135,9 +136,9 @@
                         <div class="mb-3">
                             <label for="category" class="form-label">Category</label>
                             <select name="category" class="form-select" id="category_product">
-                            @foreach ($categories as $category)
+                                @foreach ($categories as $category)
                                 <option selected value="{{$category->id}}">{{$category->name}}</option>
-                            @endforeach
+                                @endforeach
                             </select>
                         </div>
                         <button type="submit" class="btn btn-primary">Create</button>
@@ -170,18 +171,18 @@
                             <label for="price" class="form-label">Price</label>
                             <input type="text" class="form-control" id="productprice" name="price">
                         </div>
-                      <div class="mb-3">
-                        <label for="category" class="form-label">Category</label>
-                        <select name="category" id="edit_category_product" class="form-select">
-                            @foreach ($categories as $category)
-                            @if ($category->name == $product->category)
-                            <option value="{{$category->id}}" selected>{{$category->name}}</option>
-                            @else
-                            <option value="{{$category->id}}">{{$category->name}}</option>
-                            @endif
-                            @endforeach
-                        </select>
-                    </div>
+                        <div class="mb-3">
+                            <label for="category" class="form-label">Category</label>
+                            <select name="category" id="edit_category_product" class="form-select">
+                                @foreach ($categories as $category)
+                                @if (isset($product) && $category->name == $product->category)
+                                <option value="{{$category->id}}" selected>{{$category->name}}</option>
+                                @else
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>
                         <input type="hidden" name="product_id" id="product_id">
                         <div class="modal-footer">
                             <button type="submit" class="btn btn-primary">Update</button>
@@ -214,29 +215,36 @@
         </div>
     </div>
     <script>
-      function openEditModal(button) {
-    var product_id = button.getAttribute('data-productid');
-    var product_name = button.getAttribute('data-productname');
-    var product_desc = button.getAttribute('data-productdesc');
-    var product_price = button.getAttribute('data-productprice');
-    var product_category_name = button.getAttribute('data-categoryname'); // Obtener el nombre de la categoría
-    
-    document.getElementById('product_id').value = product_id;
-    document.getElementById('productname').value = product_name;
-    document.getElementById('productdesc').value = product_desc;
-    document.getElementById('productprice').value = product_price;
-    
-    // Obtener el select de categorías del modal de edición
-    var categorySelect = document.getElementById("edit_category_product");
-    
-    // Iterar sobre las opciones del select
-    for (var i = 0; i < categorySelect.options.length; i++) { // Si el valor de la opción coincide con el nombre de la categoría del producto, marcarla como seleccionada 
-        if (categorySelect.options[i].text===product_category_name) {
-        categorySelect.selectedIndex=i; break; } } var modal=new bootstrap.Modal(document.getElementById('editModal'), {
-        keyboard: false }); modal.show(); }
+        function openEditModal(button) {
+            var product_id = button.getAttribute('data-productid');
+            var product_name = button.getAttribute('data-productname');
+            var product_desc = button.getAttribute('data-productdesc');
+            var product_price = button.getAttribute('data-productprice');
+            var product_category_name = button.getAttribute('data-categoryname'); // Obtener el nombre de la categoría
+
+            document.getElementById('product_id').value = product_id;
+            document.getElementById('productname').value = product_name;
+            document.getElementById('productdesc').value = product_desc;
+            document.getElementById('productprice').value = product_price;
+
+            // Obtener el select de categorías del modal de edición
+            var categorySelect = document.getElementById("edit_category_product");
+
+            // Iterar sobre las opciones del select
+            for (var i = 0; i < categorySelect.options.length; i++) { // Si el valor de la opción coincide con el nombre de la categoría del producto, marcarla como seleccionada 
+                if (categorySelect.options[i].text === product_category_name) {
+                    categorySelect.selectedIndex = i;
+                    break;
+                }
+            }
+            var modal = new bootstrap.Modal(document.getElementById('editModal'), {
+                keyboard: false
+            });
+            modal.show();
+        }
 
 
-    
+
         function openDeleteModal(button) {
             var product_id = button.getAttribute('data-productid');
             document.getElementById('deleteProductId').value = product_id;
@@ -246,13 +254,11 @@
             modal.show();
         }
     </script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
 </body>
+
 </html>
