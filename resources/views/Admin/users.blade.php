e1caa3f8262f5b1303edf297140aebe134ea99c9
nadia356 <110473332+nadia356@users.noreply.github.com>
2024-04-04 20:40:32 -0600

    eso

--- resources/views/Admin/users.blade.php
+++ resources/views/Admin/users.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         #a {
@@ -24,7 +23,8 @@
             color: white !important;
             background-color: rgb(0, 2, 98) !important;
         }
-        .text-muted{
+
+        .text-muted {
             margin-right: 15px;
         }
 
@@ -55,8 +55,7 @@
 <body>
     <div class="container-fluid">
         <div class="d-flex mt-4">
-            <a href="{{route('AdminHome',['id'=>$id])}}"
-                id="a" class="btn btn-primary mb-2"><i class="fas fa-arrow-left"></i></a>
+            <a href="{{route('AdminHome',['id'=>$id])}}" id="a" class="btn btn-primary mb-2"><i class="fas fa-arrow-left"></i></a>
         </div>
         <h1 class="text-center mb-2">Users</h1>
         @if (session('success'))
@@ -90,20 +89,17 @@
                         <td class="text-center">{{ $usuario->email }}</td>
                         <td class="text-center">{{$usuario->phone}}</td>
                         <td class="text-center">{{ $usuario->role }}</td>
-                        @if ($usuario->role != 'Administrator')
                         <td class="text-center">
-                            <button type="button" class="btn btn-primary btn-sm edit-btn" onclick="openEditModal(this)"
-                                data-userid="{{ $usuario->id }}" data-username="{{ $usuario->name }}"
-                                data-useremail="{{ $usuario->email }}" data-userrole="{{ $usuario->role }}" data-userphone="{{$usuario->phone}}">
+                            @if ($usuario->role != 'Administrator')
+                            <button type="button" class="btn btn-primary btn-sm edit-btn" onclick="openEditModal(this)" data-userid="{{ $usuario->id }}" data-username="{{ $usuario->name }}" data-useremail="{{ $usuario->email }}" data-userrole="{{ $usuario->role }}" data-userphone="{{$usuario->phone}}">
                                 Update
                             </button>
                             <!-- Botón para abrir el modal de eliminación -->
-                            <button type="button" class="btn btn-danger btn-sm delete-btn"
-                                onclick="openDeleteModal(this)" data-userid="{{ $usuario->id }}">
+                            <button type="button" class="btn btn-danger btn-sm delete-btn" onclick="openDeleteModal(this)" data-userid="{{ $usuario->id }}">
                                 Delete
                             </button>
+                            @endif
                         </td>
-                        @else
                     </tr>
                     @endforeach
                 </tbody>
@@ -113,8 +109,7 @@
     <div class="d-flex justify-content-center">
         {{ $users->links('pagination::bootstrap-5') }}
     </div>
-    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -130,10 +125,9 @@
                             <label for="editName">Name</label>
                             <input type="text" class="form-control" id="editName" name="name">
                         </div>
-                         <div class="form-group">
+                        <div class="form-group">
                             <label for="editEmail">Email</label>
-                            <input readonly type="email" class="form-control" id="editEmail"
-                                name="email">
+                            <input readonly type="email" class="form-control" id="editEmail" name="email">
                         </div>
                         <div class="form-group">
                             <label for="editPhone">Phone</label>
@@ -144,13 +138,13 @@
                             <label for="editRole">Role</label>
                             {{-- <input type="text" class="form-control" id="editRole" name="role"> --}}
                             <select class="form-select" id="editRole" name="role">
-                                
-                                <option  value="Administrator">Administrator</option>
+
+                                <option value="Administrator">Administrator</option>
                                 <option value="Coordinator">Coordinator</option>
                                 <option value="Guest">Guest</option>
                             </select>
                         </div>
-                 
+
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary" id="a">Save changes</button>
@@ -159,10 +153,9 @@
             </form>
         </div>
     </div>
-    
+
     <!-- Modal de eliminación -->
-    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -188,8 +181,7 @@
         </div>
     </div>
     {{-- Modal de crear usuario --}}
-    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -206,8 +198,7 @@
                         </div>
                         <div class="form-group">
                             <label for="createEmail">Email</label>
-                            <input type="email" class="form-control" id="createEmail"
-                                name="email">
+                            <input type="email" class="form-control" id="createEmail" name="email">
                         </div>
                         <div class="form-group">
                             <label for="createPhone">Phone</label>
@@ -215,15 +206,14 @@
                         </div>
                         <div class="form-group">
                             <label for="createPassword">Password</label>
-                            <input type="password" class="form-control" id="createPassword"
-                              minlength="8"  name="password">
+                            <input type="password" class="form-control" id="createPassword" minlength="8" name="password">
                         </div>
                         <div class="form-group mb-4">
                             <label for="createRole">Role</label>
                             <select class="form-select" id="createRole" name="role">
                                 @foreach ($roles as $role)
-                                    <option value="{{$role->name}}">{{$role->name}}</option>
-                                    
+                                <option value="{{$role->name}}">{{$role->name}}</option>
+
                                 @endforeach
                             </select>
                         </div>
@@ -232,57 +222,63 @@
                             <button type="submit" class="btn btn-primary" id="a">Create user</button>
                         </div>
                     </form>
- <script>
-    function openEditModal(button) {
-        var userId = button.getAttribute('data-userid');
-        var userName = button.getAttribute('data-username');
-        var userEmail = button.getAttribute('data-useremail');
-        var userRole = button.getAttribute('data-userrole');
-        var userPhone = button.getAttribute('data-userphone');
+                    <script>
+                        function openEditModal(button) {
+                            var userId = button.getAttribute('data-userid');
+                            var userName = button.getAttribute('data-username');
+                            var userEmail = button.getAttribute('data-useremail');
+                            var userRole = button.getAttribute('data-userrole');
+                            var userPhone = button.getAttribute('data-userphone');
 
 
-        document.getElementById('editName').value = userName;
-        document.getElementById('editEmail').value = userEmail;
-        document.getElementById('editPhone').value = userPhone;
-        // document.getElementById('editRole').value = userRole;
-        var selectElement = document.getElementById('editRole');
+                            document.getElementById('editName').value = userName;
+                            document.getElementById('editEmail').value = userEmail;
+                            document.getElementById('editPhone').value = userPhone;
+                            // document.getElementById('editRole').value = userRole;
+                            var selectElement = document.getElementById('editRole');
 
-       
-        
-      // Verifica si ya existe una opción con el mismo valor
-        var exists = false;
-        for (var i = 0; i < selectElement.options.length; i++) { if (selectElement.options[i].value==userRole) { exists=true;
-        break; } } // Si no existe, crea y agrega la nueva opción 
-        if (!exists) { var
-        option=document.createElement('option'); option.value=userRole; option.text=userRole; option.selected=true;
-        selectElement.add(option); 
-        }
-        var editModalElement = document.getElementById('editModal');
-        var editModal = new bootstrap.Modal(document.getElementById('editModal'));
-        editModal.show();
 
-        // document.getElementById('editForm').action = 'Admin/users/' + userId;
-    }
 
-    function openDeleteModal(button) {
-        var userId = button.getAttribute('data-userid');
-        document.getElementById('deleteUser').value = userId;
-    
+                            // Verifica si ya existe una opción con el mismo valor
+                            var exists = false;
+                            for (var i = 0; i < selectElement.options.length; i++) {
+                                if (selectElement.options[i].value == userRole) {
+                                    exists = true;
+                                    break;
+                                }
+                            } // Si no existe, crea y agrega la nueva opción 
+                            if (!exists) {
+                                var
+                                    option = document.createElement('option');
+                                option.value = userRole;
+                                option.text = userRole;
+                                option.selected = true;
+                                selectElement.add(option);
+                            }
+                            var editModalElement = document.getElementById('editModal');
+                            var editModal = new bootstrap.Modal(document.getElementById('editModal'));
+                            editModal.show();
 
-        var deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
-        deleteModal.show();
+                            // document.getElementById('editForm').action = 'Admin/users/' + userId;
+                        }
 
-        // document.getElementById('deleteForm').action = '/users/' + userId;
-    }
+                        function openDeleteModal(button) {
+                            var userId = button.getAttribute('data-userid');
+                            document.getElementById('deleteUser').value = userId;
 
-</script>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-            integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-        </script>
+                            var deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+                            deleteModal.show();
+
+                            // document.getElementById('deleteForm').action = '/users/' + userId;
+                        }
+                    </script>
+
+                    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+                    </script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+                    </script>
 </body>
+
 </html>
