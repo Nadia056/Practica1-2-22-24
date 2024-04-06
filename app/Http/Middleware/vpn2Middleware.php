14ea2e629001effccd11f1a9c263f2785e3d8d23
nadia356 <110473332+nadia356@users.noreply.github.com>
2024-04-05 21:41:54 -0600

    vpn

--- app/Http/Middleware/vpn2Middleware.php
+++ app/Http/Middleware/vpn2Middleware.php
@@ -3,7 +3,13 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use PDO;
+use PDOException;
 
 class vpn2Middleware
 {
@@ -16,15 +22,31 @@ class vpn2Middleware
      */
     public function handle(Request $request, Closure $next)
     {  
+        try {
         $user = $request->user();
+
         if ($request->ip() !='192.168.1.2' && $user->role_id == 1) {
+             Auth::logout();
             return redirect()->route('login.form')->with('error', 'Invalid Credentials,');
         }
+
         else if ($request->ip() == '192.168.1.2' && $user->role_id == 3) {
+            Auth::logout();
             return redirect()->route('login.form')->with('error', 'Invalid Credentials,');
         }
        
 
         return $next($request);
+    } catch (Exception $e) {
+        Log::error('Error in vpn2Middleware' . $e);
+        return redirect()->route('login.form');
+    }catch(PDOException $e){
+        Log::error('Error in vpn2Middleware' . $e);
+        return redirect()->route('login.form');
+    }catch(QueryException $e)
+    {
+        Log::error('Error in vpn2Middleware' . $e);
+        return redirect()->route('login.form');
     }
+}
 }
